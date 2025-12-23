903d93dea28663bb90ab24ec9756826391a8c727
Cklf537 <cklf537@gmail.com>
2025-12-22 16:41:56 -0800

    sl4j support added.

--- src/main/java/com/circleback/portfolio/controller/AuthenticationController.java
+++ src/main/java/com/circleback/portfolio/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.circleback.portfolio.entity.AuthenticationResponse;
 import com.circleback.portfolio.entity.Users;
 import com.circleback.portfolio.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
